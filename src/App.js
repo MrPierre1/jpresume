7fc0257c010ad34f167f14f14c9eaf511d63d3ba
jpierrelouis <jean.pierre-louis@cengage.com>
2020-03-21 19:24:28 -0400

    first commit to repo

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import ReactGA from 'react-ga';
+import resume from './data/resumeData.json'
+import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Contact from './Components/Contact';
+import Portfolio from './Components/Portfolio';
+
+ function App () {
+     const [resumeData, setresumeData] = useState(resume)
+
+    ReactGA.initialize('UA-110570651-1');
+    ReactGA.pageview(window.location.pathname);
+
+
+  useEffect(() => {
+    // getResumeData()
+    console.log('resume data', resumeData)
+    return () => {
+    }
+  }, [resumeData])
+
+ 
+    return (
+      <div className="App">
+        <Header data={resumeData.main}/>
+        <About data={resumeData.main}/>
+        <Resume data={resumeData.resume}/>
+        <Portfolio data={resumeData.portfolio}/>
+        <Contact data={resumeData.main}/>
+        <Footer data={resumeData.main}/>
+      </div>
+    );
+}
+
+export default App;
