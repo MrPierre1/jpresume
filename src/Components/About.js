17c135d761b0e9d3b06f43d31239ca93835878b1
jpierrelouis <jean.pierre-louis@cengage.com>
2020-07-14 13:18:45 -0400

    updating title

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,30 +1,30 @@
-import React, {  } from 'react';
+import React, { } from 'react';
 
-function About (props) {
+function About(props) {
 
-      const profilepic = "images/"+props.data.image;
-      const bio = props.data.bio;
+   const profilepic = "images/" + props.data.image;
+   const bio = props.data.bio;
 
-    return (
+   return (
       <section id="about">
-      <div className="row">
-         <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Nordic Giant Profile Pic" />
-         </div>
-         <div className="nine columns main-col">
-            <h2>About Me</h2>
+         <div className="row">
+            <div className="three columns">
+               <img className="profile-pic" src={profilepic} />
+            </div>
+            <div className="nine columns main-col">
+               <h2>About Me</h2>
 
-            <p>{bio}</p>
-            <div className="row">
-               <div className="columns download">
-                 
+               <p>{bio}</p>
+               <div className="row">
+                  <div className="columns download">
+
+                  </div>
                </div>
             </div>
          </div>
-      </div>
 
-   </section>
-    );
+      </section>
+   );
 }
 
 export default About;
